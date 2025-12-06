569476182ef078e218478acf1e6dde73972bf465
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2025-12-06 22:18:05 +0530

    Remove unused imports and clean up code

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -11,7 +11,6 @@ import Footer from './components/Footer'
 import Marquee from 'react-fast-marquee'
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-
 const App = () => {
   return (
     <>
